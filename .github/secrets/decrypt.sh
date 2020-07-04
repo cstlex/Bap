1ee13f36df8819ffb2b17d3001c49978bcebaa93
MinSoo Kim <minsoo8449@gmail.com>
2020-07-04 16:56:56 +0900

    done

---
+++ .github/secrets/decrypt.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/profile.mobileprovision ./.github/secrets/profile.mobileprovision.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/dev.mobileprovision ./.github/secrets/dev.mobileprovision.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/Certificates.p12 ./.github/secrets/Certificates.p12.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/Dev.p12 ./.github/secrets/Dev.p12.gpg
+
+mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+
+cp ./.github/secrets/profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/d2cc4a78-431f-4821-aaa2-39a74188812f.mobileprovision
+cp ./.github/secrets/dev.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/ab394714-b003-417e-867e-f4432e13b3c4.mobileprovision
+
+security create-keychain -p "" build.keychain
+security import ./.github/secrets/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
+security import ./.github/secrets/Dev.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
+
+security list-keychains -s ~/Library/Keychains/build.keychain
+security default-keychain -s ~/Library/Keychains/build.keychain
+security unlock-keychain -p "" ~/Library/Keychains/build.keychain
+
+security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
+
